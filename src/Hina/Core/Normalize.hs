a30ea1edbb8e7d9710229e9864e350cad824159c
Xy Ren <xy.r@outlook.com>
2021-03-25 01:25:58 +0800

    CORE: Add substitution and normalization

---
+++ src/Hina/Core/Normalize.hs
@@ -0,0 +1,62 @@
+module Hina.Core.Normalize where
+
+import           Control.Monad.Freer       (Eff, Member)
+import           Control.Monad.Freer.State (State)
+import           Data.Maybe                (isJust)
+import           Hina.Core                 (Param (Param),
+                                            Term (TApp, TBind, TCallVar, TLam, TPi, TProj, TSigma, TTup, TUniv),
+                                            TermApp (TermApp),
+                                            TermCallVar (TermCallVar),
+                                            TermLam (TermLam),
+                                            TermProj (TermProj),
+                                            TermTup (TermTup), aTerm, dBody)
+import           Hina.Core.Substitute      (subst)
+import           Hina.Mapping              (CoreMapping, getCoreVar)
+
+isWhnf :: Member (State CoreMapping) m => Term -> Eff m Bool
+isWhnf term = case term of
+  TApp (TermApp fn _) -> case fn of
+    TLam _ -> pure True
+    _      -> isWhnf fn
+  TLam _ -> pure True
+  TPi _ -> pure True
+  TTup _ -> pure True
+  TSigma _ -> pure True
+  TProj (TermProj tup _) -> case tup of
+    TTup _ -> pure True
+    _      -> isWhnf tup
+  TUniv _ -> pure True
+  TBind _ -> pure True
+  TCallVar (TermCallVar ref) -> do
+    coreVar <- getCoreVar ref
+    pure $ isJust (dBody coreVar)
+
+normalizeToWhnf :: Member (State CoreMapping) m => Term -> Eff m Term
+normalizeToWhnf term = case term of
+  TApp (TermApp fn arg) -> case fn of
+    TLam (TermLam (Param bind _) body) ->
+      normalizeToWhnf $ subst bind (aTerm arg) body
+    _ -> do
+      fnIsWhnf <- isWhnf fn
+      if fnIsWhnf then pure term else do
+        fn' <- normalizeToWhnf fn
+        normalizeToWhnf (TApp (TermApp fn' arg))
+  TLam _ -> pure term
+  TPi _ -> pure term
+  TTup _ -> pure term
+  TSigma _ -> pure term
+  TProj (TermProj tup toLeft) -> case tup of
+    TTup (TermTup left right) ->
+      normalizeToWhnf if toLeft then left else right
+    _ -> do
+      tupIsWhnf <- isWhnf tup
+      if tupIsWhnf then pure term else do
+        tup' <- normalizeToWhnf tup
+        normalizeToWhnf (TProj (TermProj tup' toLeft))
+  TUniv _ -> pure term
+  TBind _ -> pure term
+  TCallVar (TermCallVar ref) -> do
+    coreVar <- getCoreVar ref
+    case dBody coreVar of
+      Nothing -> pure term
+      Just x  -> pure x
---
+++ src/Hina/Core/Substitute.hs
@@ -0,0 +1,42 @@
+module Hina.Core.Substitute where
+
+import qualified Data.Map.Strict as Map
+import           Hina.Core       (Arg (Arg), Param (Param),
+                                  Term (TApp, TBind, TCallVar, TLam, TPi, TProj, TSigma, TTup, TUniv),
+                                  TermApp (TermApp), TermBind (TermBind),
+                                  TermCallVar (TermCallVar), TermLam (TermLam),
+                                  TermPi (TermPi), TermProj (TermProj),
+                                  TermSigma (TermSigma), TermTup (TermTup),
+                                  TermUniv (TermUniv))
+import           Hina.Ref        (RefBind)
+
+substAll :: Map.Map RefBind Term -> Term -> Term
+substAll mapping term = case term of
+  TApp (TermApp fn arg) ->
+    TApp (TermApp (substAll mapping fn) (substAllArg mapping arg))
+  TLam (TermLam param body) ->
+    TLam (TermLam (substAllParam mapping param) (substAll mapping body))
+  TPi (TermPi param body) ->
+    TPi (TermPi (substAllParam mapping param) (substAll mapping body))
+  TTup (TermTup left right) ->
+    TTup (TermTup (substAll mapping left) (substAll mapping right))
+  TSigma (TermSigma param body) ->
+    TSigma (TermSigma (substAllParam mapping param) (substAll mapping body))
+  TProj (TermProj tup left) ->
+    TProj (TermProj (substAll mapping tup) left)
+  TUniv TermUniv ->
+    TUniv TermUniv
+  TBind (TermBind ref) -> case Map.lookup ref mapping of
+    Just x  -> x
+    Nothing -> TBind (TermBind ref)
+  TCallVar (TermCallVar ref) ->
+    TCallVar (TermCallVar ref)
+
+substAllArg :: Map.Map RefBind Term -> Arg -> Arg
+substAllArg mapping (Arg term) = Arg (substAll mapping term)
+
+substAllParam :: Map.Map RefBind Term -> Param -> Param
+substAllParam mapping (Param bind typ) = Param bind (substAll mapping typ)
+
+subst :: RefBind -> Term -> Term -> Term
+subst r t = substAll (Map.singleton r t)
