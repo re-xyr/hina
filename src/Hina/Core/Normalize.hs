3f45979976072a089bb6b160a4f673ae5899a187
Xy Ren <xy.r@outlook.com>
2021-03-27 18:07:57 +0800

    CORE: Better normalize performance

--- src/Hina/Core/Normalize.hs
+++ src/Hina/Core/Normalize.hs
@@ -2,7 +2,6 @@ module Hina.Core.Normalize where
 
 import           Control.Monad.Freer        (Eff, Member)
 import           Control.Monad.Freer.Reader (Reader)
-import           Data.Maybe                 (isJust)
 import           Hina.Core                  (Term (TApp, TBind, TCallVar, TLam, TPi, TProj, TSigma, TTup, TUniv),
                                              TermApp (TermApp),
                                              TermCallVar (TermCallVar),
@@ -12,50 +11,37 @@ import           Hina.Core                  (Term (TApp, TBind, TCallVar, TLam, 
 import           Hina.Core.Substitute       (subst)
 import           Hina.Mapping               (CoreMapping, askCoreVar)
 
-isWhnf :: Member (Reader CoreMapping) m => Term -> Eff m Bool
-isWhnf term = case term of
-  TApp (TermApp fn _) -> case fn of
-    TLam _ -> pure True
-    _      -> isWhnf fn
-  TLam _ -> pure True
-  TPi _ -> pure True
-  TTup _ -> pure True
-  TSigma _ -> pure True
-  TProj (TermProj tup _) -> case tup of
-    TTup _ -> pure True
-    _      -> isWhnf tup
-  TUniv _ -> pure True
-  TBind _ -> pure True
-  TCallVar (TermCallVar ref) -> do
-    coreVar <- askCoreVar ref
-    pure $ isJust (dBody coreVar)
-
 normalizeToWhnf :: Member (Reader CoreMapping) m => Term -> Eff m Term
-normalizeToWhnf term = case term of
+normalizeToWhnf term = fst <$> normalizeToWhnf' term
+
+normalizeToWhnf' :: Member (Reader CoreMapping) m => Term -> Eff m (Term, Bool)
+normalizeToWhnf' term = case term of
   TApp (TermApp fn arg) -> case fn of
-    TLam (TermLam bind body) ->
-      normalizeToWhnf $ subst bind (aTerm arg) body
+    TLam (TermLam bind body) -> do
+      whnf <- normalizeToWhnf $ subst bind (aTerm arg) body
+      pure (whnf, False)
     _ -> do
-      fnIsWhnf <- isWhnf fn
-      if fnIsWhnf then pure term else do
-        fn' <- normalizeToWhnf fn
-        normalizeToWhnf (TApp (TermApp fn' arg))
-  TLam _ -> pure term
-  TPi _ -> pure term
-  TTup _ -> pure term
-  TSigma _ -> pure term
+      (fn', fnIsWhnf) <- normalizeToWhnf' fn
+      if fnIsWhnf then pure (term, True) else do
+        whnf <- normalizeToWhnf (TApp (TermApp fn' arg))
+        pure (whnf, False)
+  TLam _ -> pure (term, True)
+  TPi _ -> pure (term, True)
+  TTup _ -> pure (term, True)
+  TSigma _ -> pure (term, True)
   TProj (TermProj tup toLeft) -> case tup of
-    TTup (TermTup left right) ->
-      normalizeToWhnf if toLeft then left else right
+    TTup (TermTup left right) -> do
+      whnf <- normalizeToWhnf if toLeft then left else right
+      pure (whnf, False)
     _ -> do
-      tupIsWhnf <- isWhnf tup
-      if tupIsWhnf then pure term else do
-        tup' <- normalizeToWhnf tup
-        normalizeToWhnf (TProj (TermProj tup' toLeft))
-  TUniv _ -> pure term
-  TBind _ -> pure term
+      (tup', tupIsWhnf) <- normalizeToWhnf' tup
+      if tupIsWhnf then pure (term, True) else do
+        whnf <- normalizeToWhnf (TProj (TermProj tup' toLeft))
+        pure (whnf, False)
+  TUniv _ -> pure (term, True)
+  TBind _ -> pure (term, True)
   TCallVar (TermCallVar ref) -> do
     coreVar <- askCoreVar ref
     case dBody coreVar of
-      Nothing -> pure term
-      Just x  -> pure x
+      Nothing -> pure (term, True)
+      Just x  -> pure (x, False)
